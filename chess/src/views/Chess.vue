0217deb774a6deb7270f0ee8794ffd753d1760fd
hua-bang <1964760211@qq.com>
2020-12-21 00:53:24 +0800

    基本能实现下棋功能（可能存在少许bug）

---
+++ chess/src/views/Chess.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="canvasWrap">
+      <div id="currActive">红方</div>
+      <canvas id="canvas" width="500" height="550"></canvas>
+      <div id="panel">
+        <ul id="ul"></ul>
+      </div>
+  </div>
+</template>
+
+<script>
+import Chess from "../utils/Chess"
+const chess = new Chess()
+
+export default {
+  name: "Chess",
+  data() {
+    return {
+
+    }
+  },
+  methods: {
+    init_chess(){
+      chess.init({},"canvas");
+    }
+  },
+  created() {
+
+  },
+  mounted() {
+    this.init_chess()
+  }
+}
+</script>
+
+<style scoped>
+#canvasWrap{
+  width: 600px;
+  height: 550px;
+  margin: 50px auto;
+}
+#canvas{
+  background: #EAC591;
+}
+</style>
